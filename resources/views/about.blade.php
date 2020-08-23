0c4bd7c5e1176479b18d0798717422702b516cdb
unknown <1045503@mborijnland.nl>
2020-08-23 21:19:21 +0200

    Add new views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('partials.app')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>About</h1>
+
+    <p>Lorem ipsum dolor sit amet</p>
+
+    <p>{{ $exampleString }}</p>
+@endsection
---
+++ resources/views/services.blade.php
@@ -0,0 +1,18 @@
+@extends('partials.app')
+
+@section('title', 'Services')
+
+@section('content')
+    <h1>Services</h1>
+
+    <p>Lorem ipsum dolor sit amet</p>
+
+    <ul>
+        @forelse($services as $service)
+        <li>{{ $service }}</li>
+        @empty
+        <li>No services available</li>
+        @endforelse
+    </ul>
+@endsection
+
